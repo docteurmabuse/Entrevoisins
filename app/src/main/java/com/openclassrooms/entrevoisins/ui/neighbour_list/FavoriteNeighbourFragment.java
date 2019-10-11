620db5f8469663147b86942949ef0289f90a99ae
Laurent Tizzone <laurent.tizzone@gmail.com>
2019-10-11 21:17:59 +0200

    - add cardview in detail fragment

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriteNeighbourFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriteNeighbourFragment.java
@@ -1,23 +1,22 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.openclassrooms.entrevoisins.R;
-import com.openclassrooms.entrevoisins.model.Neighbour;
 
-import java.util.List;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link FavoriteNeighbourFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class FavoriteNeighbourFragment extends Fragment {
-    private RecyclerView mFavRecyclerView;
-    private List <Neighbour> mFavoriteNeighbour;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
