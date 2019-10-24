3d377a1e9f7017deaee4c0a675a8f8adf532862d
docteurmabuse <laurent.tizzone@gmail.com>
2019-10-24 15:16:51 +0200

    modified behavior of myNeighbourLIst_LaunchDetailActivity_isWorking:  - test is working

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -2,7 +2,9 @@
 package com.openclassrooms.entrevoisins.neighbour_list;
 
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -20,12 +22,14 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
-import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 
@@ -39,6 +43,11 @@ public class NeighboursListTest {
     // This is fixed
     private static int ITEMS_COUNT = 12;
 
+    // This is fixed
+    private static int FAVOURITE_COUNT = 2;
+
+
+
     private ListNeighbourActivity mActivity;
     private DetailNeighbourActivity mDetailActivity;
 
@@ -46,13 +55,11 @@ public class NeighboursListTest {
     public ActivityTestRule<ListNeighbourActivity> mActivityRule =
             new ActivityTestRule(ListNeighbourActivity.class);
 
-    @Rule
-    public ActivityTestRule<DetailNeighbourActivity> mDetailActivityRule =
-            new ActivityTestRule(DetailNeighbourActivity.class);
 
     @Before
     public void setUp() {
         mActivity = mActivityRule.getActivity();
+        Intents.init();
         assertThat(mActivity, notNullValue());
     }
 
@@ -80,6 +87,9 @@ public class NeighboursListTest {
         onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
     }
 
+    /**
+     * We ensure that we show  detail activity when we click in recyclerview holder
+     */
     @Test
     public void myNeighbourList_LaunchDetailActivity_isWorking(){
         // Given : We show detail of the element at position 3
@@ -87,25 +97,43 @@ public class NeighboursListTest {
         // When perform click on element at position 3
         onView(ViewMatchers.withId(R.id.list_neighbours)).perform((RecyclerViewActions.actionOnItemAtPosition(3, click())));
         // Then : the detail activity is shown
-
-        //mDetailActivity = mDetailActivityRule.getActivity();
-        Intent expectedIntent = new Intent(mActivity, DetailNeighbourActivity.class);
-        assertThat(expectedIntent, notNullValue());
-        //assertNotNull("mTestActivity is null", expectedIntent);
+        intended(hasComponent(DetailNeighbourActivity.class.getName()));
     }
     /**
      * We ensure that the name of the use in detail activity is hte same than in the recyclerview holder
      */
     @Test
-    public void DetailNameText_LaunchingDetailActivity_isEqual(){
+    public void DetailNameText_LaunchingDetailActivity_isWorking(){
         // Given : We show detail of the element at position 3
         onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
         // When perform click on element at position 3
         onView(ViewMatchers.withId(R.id.list_neighbours)).perform((RecyclerViewActions.actionOnItemAtPosition(3,
                 click())));
         // Then : the detail activity is shown
-
         onView(withId(R.id.mDetailName)).check(matches(withText("Vincent")));
+    }
+    /**
+     * We ensure that the name of the use in detail activity is hte same than in the recyclerview holder
+     */
+    @Test
+    public void myFavouriteNeighbourList_LaunchingDetailActivity_isEqual(){
+        // Given : We have 3 neighbour marked as favourite
+        onView(ViewMatchers.withId(R.id.list_favorite_neighbours)).check(withItemCount(0));
+        // When perform click on element at position 3
+        onView(ViewMatchers.withId(R.id.list_neighbours)).perform((RecyclerViewActions.actionOnItemAtPosition(3, click())));
+        onView(ViewMatchers.withId(R.id.fab)).perform(click());
+        Espresso.pressBack();
+
+        onView(ViewMatchers.withId(R.id.list_neighbours)).perform((RecyclerViewActions.actionOnItemAtPosition(2, click())));
+        onView(ViewMatchers.withId(R.id.fab)).perform(click());
+        Espresso.pressBack();
+        onView(ViewMatchers.withId(R.id.tabItem2)).perform((click()));
+
+
+        // Then : the favorite list activity element is
+        onView(ViewMatchers.withId(R.id.list_favorite_neighbours)).check(withItemCount(FAVOURITE_COUNT));
+
+
     }
 
 }
